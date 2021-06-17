f238fca224a24c340241abcef0f37b6acace7ad4
zbokhari <zbokhari@alumni.usc.edu>
2021-06-17 18:02:00 -0400

    console writeline a stringlist

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using System; // https://docs.microsoft.com/en-us/dotnet/api/system?view=net-5.0
+using System.Collections.Generic; //https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic?view=net-5.0
+                                    //gives access to stringList
 
 namespace self_study_csharp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Zain's self-study in .NET 5 and C#9. I will use this repo to initially test out what I am learning then build out more complex projects as my skills advance!");
-        }
+        static void Main(string[] args) //method
+        { //beginning of statement block
+            List<string> stringList = new List<string>();
+            stringList.Add("Welcome to");
+            stringList.Add("Zain's self-study");
+            stringList.Add("in .NET 5 and C# 9.");
+            foreach (var part in stringList)
+            {
+                Console.WriteLine(part);
+            }
+            Console.WriteLine("I will use this repo to initially test out what I am learning then build out more complex projects as my skills advance!");
+        } //end of statement block
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # self-study-csharp
-revision and notes to study c# and write clean code.
-helloworld
-hello worlds
+revision and notes to study c#9, .NET 5 and write clean code.
 
